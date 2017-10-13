fa62b411b1f8c00130215922a4e4a84b5786bccb
David Brennan <ddbren@gmail.com>
2017-10-13 22:01:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop(array) {
+/*function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
@@ -8,7 +8,7 @@ function forLoop(array) {
   }
   return array
 }
-/*
+
 function whileLoop(n) {
   while (n > 0) {
     console.log(n--)
@@ -27,3 +27,11 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
 */
+
+function forLoop(array){ 
+  for(var i = 0, i < 25, i++){ 
+    if( i === 1){ array.push("I am 1 strange loop.") 
+  } else { array.push("I am ${i} strange loops.") 
+} 
+} return array 
+}
