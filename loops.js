659698db5ea723c23a34bb9b64f9e8fbe5553066
David Brennan <ddbren@gmail.com>
2017-10-13 22:01:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -28,8 +28,8 @@ function maybeTrue() {
 }
 */
 
-function forLoop(array){
-  for(var i = 0, i < 25, i++){
+function forLoop(array) {
+  for(var i = 0; i < 25; i++){
     if( i === 1){ array.push("I am 1 strange loop.")
   } else { array.push("I am ${i} strange loops.")
 }
